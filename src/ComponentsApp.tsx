0ea62d7006fea2d3d3a3e9e14007ce9dd6302012
Mauri <mauri.monzon91@gmail.com>
2024-08-02 15:13:29 -0300

    Base

---
+++ src/ComponentsApp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const ComponentsApp = () => {
+  return (
+    <View>
+      <Text>ComponentsApp</Text>
+    </View>
+  );
+};

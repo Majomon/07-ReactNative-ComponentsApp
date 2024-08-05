f2082419dbeec1c733a53d41860bdfcf6abd9486
Mauri <mauri.monzon91@gmail.com>
2024-08-05 20:52:20 -0300

    Infinite scroll con imagenes

--- src/presentation/screens/ui/InfiniteScrollScreen.tsx
+++ src/presentation/screens/ui/InfiniteScrollScreen.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import {FlatList, Text, View} from 'react-native';
-import {CustomView} from '../../components/ui/CustomView';
+import {ActivityIndicator, FlatList, Image, View} from 'react-native';
 import {Title} from '../../components/ui/Title';
 import {colors} from '../../../config/theme/theme';
 
@@ -17,9 +16,7 @@ export const InfiniteScrollScreen = () => {
   };
 
   return (
-    <CustomView margin>
-      <Title text="Infinite Scroll" />
-
+    <View style={{backgroundColor: 'black'}}>
       <FlatList
         data={numbers}
         /* Cuando llegamos al final */
@@ -27,19 +24,28 @@ export const InfiniteScrollScreen = () => {
         /* Cargar los elementos antes de llegar al final */
         onEndReachedThreshold={0.6}
         keyExtractor={item => item.toString()}
-        
-        renderItem={({item}) => (
-          <Text
-            style={{
-              height: 300,
-              backgroundColor: colors.primary,
-              color: 'white',
-              fontSize: 50,
-            }}>
-            {item}
-          </Text>
+        renderItem={({item}) => <ListItem number={item} />}
+        /* Indicador de carga */
+
+        ListFooterComponent={() => (
+          <View style={{height: 150, justifyContent: 'center'}}>
+            <ActivityIndicator size={40} color={colors.primary} />
+          </View>
         )}
       />
-    </CustomView>
+    </View>
+  );
+};
+
+interface ListItemProps {
+  number: number;
+}
+
+const ListItem = ({number}: ListItemProps) => {
+  return (
+    <Image
+      source={{uri: `https://picsum.photos/id/${number}/200/300`}}
+      style={{height: 400, width: '100%'}}
+    />
   );
 };

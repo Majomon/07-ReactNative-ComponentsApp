dfcf757210d007050adfd3937fba6ae41e69fba8
Mauri <mauri.monzon91@gmail.com>
2024-08-04 22:30:34 -0300

    Custom button

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {CustomView} from '../../components/ui/CustomView';
-import {Card} from '../../components/ui/Card';
-import {Button} from '../../components/ui/Button';
 
 export const SwitchScreen = () => {
   return (
     <CustomView style={{marginTop: 100, marginHorizontal: 10}}>
-      <Card>
-        {/*  <Text>SwitchScreen</Text> */}
-        <Button text="Click me" onPress={() => {}} />
-      </Card>
+
+      
     </CustomView>
   );
 };
